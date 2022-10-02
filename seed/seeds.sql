0b88e0bf46798f3a3ba7299083ab49839f6e5c86
cwendland <chris.r.wendland@gmail.com>
2022-10-02 15:55:27 -0700

    Added addEmployee function

---
+++ seed/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees(first_name, last_name, manager_id, position_id)
+    VALUES ('John', 'Hamm', 1, 3),
+           ('Mary', 'Johnson', 1, 3),
+           ('Frank', 'Reynolds', , 3);
